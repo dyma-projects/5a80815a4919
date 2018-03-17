15dc1cda4fa7d52ad592a106c5ecf94926287e53
wattaa90 <c.massi@live.fr>
2018-03-17 20:23:41 +0100

    Commit Fin projet

--- src/app/directive/directive.component.ts
+++ src/app/directive/directive.component.ts
@@ -10,11 +10,27 @@ export class DirectiveComponent implements OnInit {
 
     nom: string;
     typeInput: string;
+    status: boolean;
+    p1: string;
+    p2: string;
+    texts: Array<string>;
+
 
     constructor() {
         this.nom ='Massine';
-        this.typeInput = 'number'
+        this.typeInput = 'number';
+        this.status = true;
+        this.p1 ="Paragraphe 1";
+        this.p2 ="Paragraphe 2";
+        this.texts = ["Un", "Deux", "Trois"];
     }
+
+    controlerStatus() {
+        console.log("Directive Click !");
+         this.status = ! this.status;
+        console.log(this.status);
+    }
+
 
     afficherInput(val : string) {
       console.log(val);
